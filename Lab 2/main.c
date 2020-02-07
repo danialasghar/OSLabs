f38ee28197ff1f9748ff7760731802530003c4c0
pawanp1808 <patelpawan1996@hotmail.com>
2020-02-07 15:46:13 -0500

    tokeniser

--- Lab 2/main.c
+++ Lab 2/main.c
@@ -17,7 +17,7 @@
 
 int exit_flag = 0;
 
-void exit(){
+void exit_shell(){
     exit_flag=1;
 }
 
@@ -32,35 +32,86 @@ void pause_shell() {
     getchar();
 }
 
-void process_input(char *str){
-//    char command[256];
+void process_tokens(char **tokens, int size) {
+
+        if(strcmp(tokens[0],"cd")==0){
+            printf("cd");
+            
+        } else if (strcmp(tokens[0],"clr")==0) {
+            clr();
+            
+        } else if (strcmp(tokens[0],"dir")==0) {
+            printf("dir");
+            
+        }else if (strcmp(tokens[0],"environ")==0) {
+            printf("env");
+            
+        }else if (strcmp(tokens[0],"echo")==0) {
+            printf("echo");
+            
+        }else if (strcmp(tokens[0],"help")==0) {
+             printf("help");
+            
+        }else if (strcmp(tokens[0],"pause")==0) {
+            pause_shell();
+            
+        }else if (strcmp(tokens[0],"quit")==0) {
+            exit_shell();
+            
+        }
+        
+    
+    
+
+
+    
+}
+
+void tokenise_input(char *str){
     char* token;
-//    strcpy(command,buff);
-       char delim[1] = " ";
+    char delim[1] = " ";
+    char ** tokenisedStr;
      token = strtok(str, delim);
+    
+    int i=0;
      
      while(token!=NULL){
-         printf("%s\n",token );
-         
-         
-         
-         
+       
+         tokenisedStr[i] = token;
+         i++;
          token = strtok(NULL,delim);
          
      }
+    //i was incremented an extra time when for loop exited
+    process_tokens(tokenisedStr,i--);
 }
 
-void readInput(){
+int readInput(){
     char * buff = readline("\n");
-    process_input(buff);
+    if(strlen(buff)==0){
+        return 1;
+    } else {
+         tokenise_input(buff);
+        return 0;
+    }
+   
 }
 
 int main(int argc, const char * argv[]) {
     while(1 & !exit_flag){
+
+//        if(readInput()) {
+//            continue;
+//        }
+//
+        char *c = malloc(sizeof(c)*100);
+        scanf("%s",c);
+        tokenise_input(c);
       
-        readInput();
-        
-        
+
+
     }
+//
+    printf("sadfasdf");
     return 0;
 }
